f3624b8f99b6be02dc971c93b405cf8695409aac
7R0N1X <techsoftec.tronix@gmail.com>
2024-05-10 15:27:50 -0500

    se agrega sección de servicios

---
+++ src/js/servicios.js
@@ -0,0 +1,17 @@
+export const SERVICIOS = [
+  {
+    title: "Emergencias Médicas",
+    options: ["Caso de emergencia", "Urgencia médica", "Situación crítica", "Atención inmediata", "Emergencia médica"],
+    link: "#"
+  },
+  {
+    title: "Atención Oncológica",
+    options: ["Cuidado del cáncer", "Tratamiento oncológico", "Apoyo a pacientes con cáncer", "Atención cáncer especializada", "Oncología"],
+    link: "#"
+  },
+  {
+    title: "Consultas de Salud",
+    options: ["Preguntas sobre salud", "Dudas médicas", "Consultas médicas", "Información de salud", "Asesoramiento médico"],
+    link: "#"
+  },
+];
