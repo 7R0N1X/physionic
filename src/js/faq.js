161b35a16da6b198a548cdbe6932c7dd9718995b
7R0N1X <techsoftec.tronix@gmail.com>
2024-05-12 13:10:32 -0500

    se agrega sección de FAQ

---
+++ src/js/faq.js
@@ -0,0 +1,26 @@
+export const FAQ = [
+  {
+    question: "¿Cuáles son sus horas de atención?",
+    answer: "Nuestro horario de atención es de lunes a viernes de 9:00 a.m. a 5:00 p.m."
+  },
+  {
+    question: "¿Aceptan seguro médico?",
+    answer: "Sí, aceptamos la mayoría de los seguros médicos. Por favor, consulte con su proveedor de seguros para obtener más información."
+  },
+  {
+    question: "¿Qué llevar a la primera consulta?",
+    answer: "Por favor, traiga su tarjeta de seguro médico, identificación y cualquier información médica relevante."
+  },
+  {
+    question: "¿Cuál es su política de cancelación?",
+    answer: "Por favor, comuníquese con nosotros con al menos 24 horas de anticipación si necesita cancelar o reprogramar su cita."
+  },
+  {
+    question: "¿Ofrecen servicios de emergencia?",
+    answer: "Sí, ofrecemos servicios de emergencia las 24 horas del día, los 7 días de la semana."
+  },
+  {
+    question: "¿Ofrecen atención domiciliaria?",
+    answer: "Sí, ofrecemos servicios de atención domiciliaria para pacientes que no pueden venir a nuestra clínica."
+  },
+]
